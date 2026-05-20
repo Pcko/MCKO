62df0f00c189c3f27d0cbcf7dd83fcaf0349ce56
Pcko <126863503+Pcko@users.noreply.github.com>
2026-05-20 22:02:53 +0200

    feature: add basic webservice and template prototype

---
+++ scripts/start_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ -z "$MC_SERVER_DIR" ]; then
+    echo "MC_SERVER_DIR is not set"
+    exit 1
+fi
+
+cd "$MC_SERVER_DIR"
+
+java -Xms2G -Xmx6G -jar server.jar nogui
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod template;
 
-use std::env;
+use std::{env, net::{SocketAddr, TcpStream}, path::PathBuf, process::Command, time::Duration};
 
 use dotenv::dotenv;
-use axum::{Router, response::IntoResponse, routing::{get, post}};
+use axum::{Form, Router, http::{StatusCode, header}, response::IntoResponse, routing::{get, post}};
+use serde::Deserialize;
 use tower_http::services::ServeDir;
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
@@ -18,7 +19,7 @@ async fn main() {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "with_axum_htmx_askama=debug".into()),
+                .unwrap_or_else(|_| "debug".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
@@ -39,11 +40,92 @@ fn app() -> Router{
 
     Router::new()
         .route("/",get(dashboard))
-        //.route("/start", post(|| "Start".into()))
-        //.route("stop", post(|| "Stop".into()))
+        .route("/start", post(start))
+        .route("/status", get(status))
         .nest_service("/assets",ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())) )
 }
 
 async fn dashboard() -> impl IntoResponse{
     HtmlTemplate(DashboardTemplate{})
+}
+
+#[derive(Deserialize)]
+struct FormData {
+    secret: String,
+}
+
+async fn start(Form(data): Form<FormData>) -> impl IntoResponse {
+    let env_secret = match env::var("MC_SECRET") {
+        Ok(value) => value,
+        Err(_) => {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                [(header::CACHE_CONTROL, "no-store")],
+                r#"<div class="error">Server secret is not configured.</div>"#,
+            );
+        }
+    };
+
+    if data.secret != env_secret {
+        return (
+            StatusCode::FORBIDDEN,
+            [(header::CACHE_CONTROL, "no-store")],
+            r#"<div class="error">Invalid secret.</div>"#,
+        );
+    }
+
+    if is_server_running(){
+        info!("MC Server already running...");
+        return (
+            StatusCode::FORBIDDEN,
+            [(header::CACHE_CONTROL, "no-store")],
+            r#"<div class="error">Already running.</div>"#,
+        );
+    }
+
+    // Use the default script if env var is not set
+    let mut command = Command::new("bash");
+    let env_result = env::var("MC_START_SCRIPT");
+    let server_dir = env::var("MC_SERVER_DIR").unwrap();
+
+    if let Ok(script_name) = env_result {
+        command.arg(format!("{server_dir}/{script_name}"));
+    } else {
+        let root = PathBuf::from(".");
+        let script_path = root.join("scripts").join("start_server.sh");
+        command.arg(script_path);
+    }
+    
+    match command.spawn() {
+        Ok(_) => {
+            info!("MC server start command spawned");
+
+            (
+                StatusCode::OK,
+                [(header::CACHE_CONTROL, "no-store")],
+                r#"<div class="success">Server start requested.</div>"#,
+            )
+        }
+        Err(err) => {
+            tracing::error!("Failed to execute command: {err}");
+
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                [(header::CACHE_CONTROL, "no-store")],
+                r#"<div class="error">Failed to start server.</div>"#,
+            )
+        }
+    }
+}
+
+async fn status() -> impl IntoResponse {
+
+}
+
+fn is_server_running() -> bool {
+    let addr: SocketAddr = "127.0.0.1:25565"
+        .parse()
+        .expect("invalid address");
+
+    TcpStream::connect_timeout(&addr, Duration::from_secs(1)).is_ok()
 }
