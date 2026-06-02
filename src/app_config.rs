3ee046b931d6b3938a28706b719dc7542897b0ce
Pcko <126863503+Pcko@users.noreply.github.com>
2026-06-02 16:55:10 +0200

    refactor: make env var error text more accurate

--- src/app_config.rs
+++ src/app_config.rs
@@ -19,7 +19,7 @@ impl AppConfig {
         AppConfig {
             server_host: dotenv::var("SERVER_HOST").unwrap_or_else(|_| "127.0.0.1".to_string()),
             server_port: dotenv::var("SERVER_PORT").unwrap_or_else(|_| "3000".to_string()),
-            secret_hash: dotenv::var("SECRET_HASH").expect("SECRET must be set"),
+            secret_hash: dotenv::var("SECRET_HASH").expect("SECRET_HASH must be set"),
             mc_host: dotenv::var("MC_HOST").unwrap_or_else(|_| "127.0.0.1".to_string()),
             mc_port: dotenv::var("MC_PORT").unwrap_or_else(|_| "25565".to_string()),
             mc_server_dir: dotenv::var("MC_SERVER_DIR").expect("MC_SERVER_DIR must be set"),
